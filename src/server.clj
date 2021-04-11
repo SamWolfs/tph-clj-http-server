dcb8d582481f890f919f63e0dedc4290e49346f8
Sam Wolfs <be.samwolfs@gmail.com>
2021-04-11 15:58:52 +0200

    chapter5 - read from socket

--- src/server.clj
+++ src/server.clj
@@ -2,11 +2,20 @@
   (:require [clojure.java.io :as io])
   (:import [java.net ServerSocket]))
 
+(defn client-send [socket msg]
+  (let [client (io/writer socket)]
+    (.write client msg)
+    (.flush client)))
+
+(defn client-receive [socket]
+  (let [client (io/reader socket)]
+    (.readLine client)))
 
 (defn main []
   (with-open [server-socket (ServerSocket. 9999 1)
-              socket (.accept server-socket)
-              o (io/writer socket)]
-    (.write o "Hello World!")))
+              socket (.accept server-socket)]
+    (client-send socket "Welcome!\n")
+    (let [msg (client-receive socket)]
+      (client-send socket msg))))
 
 (main)
