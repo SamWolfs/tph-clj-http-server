e50de7a44198e36f3e8dac300ab52f2a31c0cf84
Sam Wolfs <be.samwolfs@gmail.com>
2021-04-11 14:50:02 +0200

    chapter4 - writing to socket

--- src/server.clj
+++ src/server.clj
@@ -1,9 +1,12 @@
 (ns server
+  (:require [clojure.java.io :as io])
   (:import [java.net ServerSocket]))
 
 
 (defn main []
-  (let [socket (ServerSocket. 6789 2)]
-    (.accept socket)))
+  (with-open [server-socket (ServerSocket. 9999 1)
+              socket (.accept server-socket)
+              o (io/writer socket)]
+    (.write o "Hello World!")))
 
 (main)
